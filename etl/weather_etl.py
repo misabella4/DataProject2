c05e7c4d043a8e3021bfb227846c0195b3c2026b
misabella4 <vzf2re@virginia.edu>
2025-05-05 12:35:21 -0400

    ETL pipeline established with weather dataset from CSV on Kaggle

---
+++ etl/weather_etl.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+
+def extract_data(file_path):
+    # Loads raw weather data from the CSV file to use in implementation
+    df = pd.read_csv(file_path)
+    return df
+
+def prepare_data(df):
+    # Transform, simplify, and clean up raw data
+    df = df[['country', 'location_name', 'temperature_celsius', 'uv_index']].copy()
+    df = df.rename(columns={
+        'location_name': 'city',
+        'temperature_celsius': 'temp',
+        'uv_index': 'uv'
+    })
+    df.dropna(subset=['country', 'city', 'temp', 'uv'], inplace=True)
+    return df
+
+def load_data(df, output_path='data/local_weather.csv'):
+    # Save new, cleaned data to a more organized CSV
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+
+def run():
+    file_path = 'data/raw_global_weather.csv'
+    df = extract_data(file_path)
+    new_df = prepare_data(df)
+    load_data(new_df, output_path='data/local_weather.csv')
+
+if __name__ == '__main__':
+    run()
